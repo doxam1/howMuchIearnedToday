5af27139673c5ba51bc8c26276a5d43152af8526
Doxam1 <143098976+doxam1@users.noreply.github.com>
2024-05-05 20:00:10 +0300

    weekend included in the new calculator

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,55 @@
 import "../styles/style.css";
 
 const submitBtn = document.querySelector(".submitBtn");
+const saturday = document.querySelector("#shabat");
+const saturdayNight = document.querySelector("#saturdayNight");
+const fieldset = document.querySelector("fieldset");
+
+// if (saturday.checked == true) {
+saturday.onchange = () => {
+  const shabesJobs = document.createElement("div");
+  const shabesHours = document.createElement("div");
+  if (saturday.checked == true) {
+    shabesJobs.innerHTML = `
+  <label for="shabesJobsAmount">  כמה משלוחים נעשו בשבת? </label>
+  <input type="tel" id="shabesJobsAmount" name="shabesJobsAmount" placeholder='מ16:00 ועד מוצאי שבת'/>
+  `;
+    shabesJobs.classList.add("inputDiv", "shabesJobs");
+    shabesHours.innerHTML = `
+  <label for="shabesHours"> כמה שעות עבדת בשבת? </label>
+  <input type="tel" id="shabesHours" name="shabesHours" placeholder='כניסת השבת ועד מוצאי שבת'/>
+  `;
+    shabesHours.classList.add("inputDiv", "shabesHours");
+    fieldset.insertBefore(
+      shabesHours,
+      document.querySelector(".inputDiv.submitBtnDiv")
+    );
+    fieldset.insertBefore(
+      shabesJobs,
+      document.querySelector(".inputDiv.submitBtnDiv")
+    );
+  } else if (saturday.checked == false) {
+    fieldset.removeChild(document.querySelector(".shabesHours"));
+    fieldset.removeChild(document.querySelector(".shabesJobs"));
+  }
+};
+
+saturdayNight.onchange = () => {
+  const saturdayNightJobs = document.createElement("div");
+  if (saturdayNight.checked == true) {
+    saturdayNightJobs.classList.add("inputDiv", "saturdayNightJobs");
+    saturdayNightJobs.innerHTML = `
+    <label for="saturdayNightJobsAmount">  כמה משלוחים נעשו<b> בצאת</b> שבת? </label>
+    <input type="tel" id="saturdayNightJobsAmount" name="saturdayNightJobsAmount" placeholder='צאת שבת(20:00 בקיץ) עד הסוף'/>
+    `;
+    fieldset.insertBefore(
+      saturdayNightJobs,
+      document.querySelector(".inputDiv.submitBtnDiv")
+    );
+  } else if (saturdayNight.checked == false) {
+    fieldset.removeChild(document.querySelector(".saturdayNightJobs"));
+  }
+};
 
 submitBtn.onclick = () => {
   const jobsAmount = document.querySelector("#jobsAmount");
@@ -19,7 +68,10 @@ submitBtn.onclick = () => {
     return;
   }
 
-  if (jobsAmount.value == '' || Number.isNaN(parseInt(jobsAmount.value)) == true) {
+  if (
+    jobsAmount.value == "" ||
+    Number.isNaN(parseInt(jobsAmount.value)) == true
+  ) {
     outputDiv.style.color = "red";
     outputDiv.textContent = " כמה משלוחים עשית? ";
     setTimeout(() => {
@@ -28,7 +80,7 @@ submitBtn.onclick = () => {
     return;
   }
 
-  if (hours.value == '' || Number.isNaN(parseInt(hours.value)) == true) {
+  if (hours.value == "" || Number.isNaN(parseInt(hours.value)) == true) {
     outputDiv.style.color = "red";
     outputDiv.textContent = "כמה שעות עבדת?";
     setTimeout(() => {
@@ -37,7 +89,7 @@ submitBtn.onclick = () => {
     return;
   }
 
-  if (km.value == '' || Number.isNaN(parseInt(km.value)) == true) {
+  if (km.value == "" || Number.isNaN(parseInt(km.value)) == true) {
     outputDiv.style.color = "red";
     outputDiv.textContent = 'כמה ק"מ נסעת?';
     setTimeout(() => {
@@ -46,7 +98,6 @@ submitBtn.onclick = () => {
     return;
   }
 
-
   if (vehicle.value == "bicycle") {
     km = km.value * 0.5;
   } else if (vehicle.value == "motorbike") {
@@ -54,14 +105,150 @@ submitBtn.onclick = () => {
   }
 
   if (jobsAmount.value / hours.value >= 2) {
-    total = +(20 * jobsAmount.value + km)
+    let shabesJobsAmountMoneyWise = 0;
+    let saturdayNightJobsMoneyWise = 0;
+    if (saturday.checked == true) {
+      let shabesJobsAmountTotal =
+        document.querySelector("#shabesJobsAmount").value;
+      if (
+        shabesJobsAmountTotal == "" ||
+        Number.isNaN(parseInt(shabesJobsAmountTotal)) == true ||
+        parseInt(shabesJobsAmountTotal) > parseInt(jobsAmount.value)
+      ) {
+        outputDiv.style.color = "red";
+        outputDiv.textContent = "כמה משלוחים נעשו בשבת?";
+        setTimeout(() => {
+          outputDiv.style = "";
+          outputDiv.textContent = "";
+        }, 1500);
+        return;
+      }
+      if (saturdayNight.checked == true && saturday.checked == true) {
+        if (
+          parseInt(shabesJobsAmountTotal) +
+          parseInt(
+            document.querySelector("#saturdayNightJobsAmount").value >
+              parseInt(jobsAmount.value)
+          )
+        ) {
+          outputDiv.style.color = "red";
+          outputDiv.textContent =
+            "כמות המשלוחים בשבת ומוצאי שבת גדולה מכמות המשלוחים הכללית, אנא בדוק שנית";
+          setTimeout(() => {
+            outputDiv.style = "";
+            outputDiv.textContent = "";
+          }, 2500);
+          return;
+        }
+      }
+      shabesJobsAmountMoneyWise = shabesJobsAmountTotal * 20;
+    }
+    if (saturdayNight.checked == true) {
+      let saturdayNightJobsAmount = document.querySelector(
+        "#saturdayNightJobsAmount"
+      ).value;
+      if (
+        saturdayNightJobsAmount == "" ||
+        Number.isNaN(parseInt(saturdayNightJobsAmount)) == true ||
+        parseInt(saturdayNightJobsAmount) > parseInt(jobsAmount.value)
+      ) {
+        outputDiv.style.color = "red";
+        outputDiv.textContent = "כמה משלוחים נעשו במוצאי שבת?";
+        setTimeout(() => {
+          outputDiv.style = "";
+          outputDiv.textContent = "";
+        }, 2500);
+        return;
+      }
+      if (saturdayNight.checked == true && saturday.checked == true) {
+        if (
+          parseInt(saturdayNightJobsAmount) +
+            parseInt(document.querySelector("#shabesJobsAmount").value) >
+          parseInt(jobsAmount.value)
+        ) {
+          outputDiv.style.color = "red";
+          outputDiv.textContent =
+            "כמות המשלוחים בשבת ומוצאי שבת גדולה מכמות המשלוחים הכללית, אנא בדוק שנית";
+          setTimeout(() => {
+            outputDiv.style = "";
+            outputDiv.textContent = "";
+          }, 1500);
+          return;
+        }
+      }
+      saturdayNightJobsMoneyWise = saturdayNightJobsAmount * 10;
+    }
+    total = +(
+      20 * jobsAmount.value +
+      km +
+      shabesJobsAmountMoneyWise +
+      saturdayNightJobsMoneyWise
+    );
   } else if (jobsAmount.value / hours.value < 2) {
-    total = +((32.3 * hours.value) + km)
+    let shabesHoursMoneyWise = 0;
+    if (saturdayNight.checked == true && saturday.checked == true) {
+      if (
+        parseInt(document.querySelector("#saturdayNightJobsAmount").value) +
+          parseInt(document.querySelector("#shabesJobsAmount").value) >
+        parseInt(jobsAmount.value)
+      ) {
+        outputDiv.style.color = "red";
+        outputDiv.textContent =
+          "כמות המשלוחים בשבת ומוצאי שבת גדולה מכמות המשלוחים הכללית, אנא בדוק שנית";
+        setTimeout(() => {
+          outputDiv.style = "";
+          outputDiv.textContent = "";
+        }, 1500);
+        return;
+      }
+    }
+    if (saturday.checked == true) {
+      let shabesHoursAmountTotal = document.querySelector("#shabesHours").value;
+      if (
+        shabesHoursAmountTotal == "" ||
+        Number.isNaN(parseInt(shabesHoursAmountTotal)) == true ||
+        parseInt(shabesHoursAmountTotal) > parseInt(hours.value)
+      ) {
+        outputDiv.style.color = "red";
+        outputDiv.textContent =
+          "כמה שעות עבדת בשבת? - כניסת שבת ועד צאת שבת בלבד";
+        setTimeout(() => {
+          outputDiv.style = "";
+          outputDiv.textContent = "";
+        }, 1500);
+        return;
+      }
+      shabesHoursMoneyWise = shabesHoursAmountTotal * 16;
+    }
+    total = +(32.3 * hours.value + km + shabesHoursMoneyWise);
+
+    if (saturday.checked == true && saturdayNight.checked == false) {
+      if ((total < document.querySelector("#shabesJobsAmount").value * 20) + jobsAmount.value * 20) {
+        total = document.querySelector("#shabesJobsAmount").value * 40 + ((jobsAmount.value - document.querySelector("#shabesJobsAmount").value) * 20);
+      }
+    } else if (saturday.checked == false && saturdayNight.checked == true) {
+      if (
+        total <
+        (document.querySelector("#saturdayNightJobsAmount").value * 10) + (jobsAmount.value * 20)
+      ) {
+        total = document.querySelector("#saturdayNightJobsAmount").value * 30 + ((jobsAmount.value - document.querySelector("#saturdayNightJobsAmount").value) * 20);
+      }
+    } else if (saturday.checked == true && saturdayNight.checked == true) {
+      if (
+        total <
+        (document.querySelector("#shabesJobsAmount").value * 20) +
+          (document.querySelector("#saturdayNightJobsAmount").value * 10) + (jobsAmount.value * 20)
+      ) {
+        total =
+          (document.querySelector("#shabesJobsAmount").value * 40) +
+          (document.querySelector("#saturdayNightJobsAmount").value * 30) +((jobsAmount.value - document.querySelector("#shabesJobsAmount").value - document.querySelector("#saturdayNightJobsAmount").value) * 20);
+      }
+    }
   }
-
-  outputDiv.style.border = '1px solid blue';
-  outputDiv.style.color = 'black';
-  outputDiv.style.padding = '1rem';
-  outputDiv.textContent = `שכר המשמרת לפי הנתונים הנ"ל הינו: ${total.toFixed(2)} ש"ח`;
+  outputDiv.style.border = "1px solid blue";
+  outputDiv.style.color = "black";
+  outputDiv.style.padding = "1rem";
+  outputDiv.textContent = `שכר המשמרת לפי הנתונים הנ"ל הינו: ${total.toFixed(
+    2
+  )} ש"ח`;
 };
-
